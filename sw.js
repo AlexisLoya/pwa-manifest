4ec32803a9e86446b8fdf77d6d6d8d795e1244ef
AlexisLoya <alexloy117@gmail.com>
2022-10-20 16:48:10 -0500

    add another strategy

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 //Only cache strategy
-const INMUTABLE_CACHE_NAME = 'inmutable-cache-v1'
+const INMUTABLE_CACHE_NAME = 'inmutable-cache-v2'
 const DYNAMIC_CACHE_NAME = 'dinamyc-cache-v1'
-const STATIC_PERSONS_NAME='static-person-cache-v1'
 const STATIC_CACHE_NAME='static-cache-v1'
 
 self.addEventListener('install',(event)=>{
@@ -23,22 +22,71 @@ self.addEventListener('install',(event)=>{
         return cache_static.addAll([
             './',
             './index.html',
+            './pages/offline.html',
             './manifest.json',
-            './images/imgjpg.jpg',
-            'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/webfonts/fa-solid-900.woff2',
-            
-
+            './images/default.png',            
         ])
     })
 
     event.waitUntil(Promise.all([inmutablePromise, staticPromise]))
 })
+// ONLY CACHE
+// self.addEventListener('fetch',(event)=>{
+//     const responseNetworkCall = caches.match(event.request).then((cacheResponse)=>{
+//         if(cacheResponse){
+//             return cacheResponse
+//         }
+//     })
+//     event.respondWith(responseNetworkCall)
+// })
 
-self.addEventListener('fetch',(event)=>{
-    const responseNetworkCall = caches.match(event.request).then((cacheResponse)=>{
-        if(cacheResponse){
-            return cacheResponse
+//update file when manifest is changed
+self.addEventListener('activate', (event) => {
+    const response = caches.keys().then((items)=>{
+        items.forEach((item)=>{
+            if(item !== STATIC_CACHE_NAME && key.includes('static')){
+                return caches.delete(item)
+            }
+        })
+    })
+    event.waitUntil(response)
+})
+
+// clean cache
+const cleanCache = (cache, limitItems)=>{
+    caches.open(cache).then((cache)=>{
+        return cache.keys().then((keys)=>{
+            if(keys.length >limitItems){
+                cache.delete(keys[0]).then(cleanCache(cache,limitItems))
+            }
+        })
+    })
+}
+
+// Only cache estrategy
+self.addEventListener("fetch",(event)=>{
+    const resp = caches.match(event.request).then((resp_cache)=>{
+        // cache
+        if (resp_cache){
+            return resp_cache
+        }
+        // red
+        return fetch(event.request).then((network_resp)=>{
+            caches.open(DYNAMIC_CACHE_NAME).then((cache)=>{
+                cache.put(event.request,network_resp)
+                cleanCache(DYNAMIC_CACHE_NAME,10)
+            })    
+            return network_resp.clone()
+        })
+    }).catch(err=>{
+        // Return html
+        if(event.request.headers.get('accept').includes('text/html')){
+            return caches.match("/pages/offline.html")
+        }
+        // Retun img
+        if(event.request.headers.get('accept').includes("image/")){
+            return caches.match("./images/default.png")
         }
     })
-    event.respondWith(responseNetworkCall)
+    event.respondWith(resp)
 })
